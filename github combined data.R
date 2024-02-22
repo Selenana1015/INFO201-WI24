44c4d7e462ce69579f3216765ad61d66b34d820b
rtandy03 <159201752+rtandy03@users.noreply.github.com>
2024-02-21 22:50:39 -0800

    Add files via upload

---
+++ github combined data.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(stringr)
+library(testthat)
+library(jsonlite)
+
+national_park_name <- read_csv("info 201/national_parks.csv")
+national_ana <- fromJSON("https://query.data.world/s/mfrcsythc5dh53b4dahsm2a37j6rjn?dws=00000") # ana- analysis
+View(national_ana)
+head(national_park_name)
+colnames(national_park_name)
+
+head(national_ana)
+colnames(national_ana)
+
+merged_data <- inner_join(national_park_name, national_ana, by = "parks")
+merged_data <- inner_join(national_park_name, national_ana, by = c("national_park_name" = "national_ana"))
+
+colnames(national_park_name)[colnames(national_park_name) == "Acadia"] <- "title"
+View(national_park_name)
+
+combined.dy <- merge(national_park_name, national_ana, by = "title")
